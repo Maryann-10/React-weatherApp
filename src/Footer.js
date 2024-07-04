e2b98475efd15b6a4eceb709eb45fb97fd349029
Maryann <maryannichaura@gmail.com>
2024-07-04 22:38:34 +0300

    Updated footer url

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,8 @@ function Footer(){
         <footer>
         This Project is Coded by
         <a href="https://github.com/" target="-blank">Maryann-10</a> and is 
-        <a href="https://github.com/Maryann-10/weather-forecast" target="blank">on GitHub</a> and 
-        <a href="https://bright-flan-c535c0.netlify.app/" target="blank">hosted on netlify</a>
+        <a href="https://github.com/Maryann-10/React-weatherApp" target="blank">on GitHub</a> and 
+        <a href="https://rococo-flan-7c6978.netlify.app/" target="blank">hosted on netlify</a>
     </footer>
     );
 }
